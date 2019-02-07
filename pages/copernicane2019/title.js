7f60e7cc8e0d8a5278fa0781a978d550443f46f1
Federico Grandi <fgrandi30@gmail.com>
2019-02-07 21:33:16 +0100

    Update title.js

--- pages/copernicane2019/title.js
+++ pages/copernicane2019/title.js
@@ -1,6 +1,6 @@
-var script = document.createElement('script');
-script.src = '//code.jquery.com/jquery-1.11.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
+var $ = document.createElement('script');
+$.src = '//code.jquery.com/jquery-1.11.0.min.js';
+document.getElementsByTagName('head')[0].appendChild($);
 
 $().ready(() => {
   $('.project-name').first().text("Copernicane 2019");
