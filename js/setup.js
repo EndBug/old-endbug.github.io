96a3a624445c29dfae73c84e0ac1c874639fe467
Federico Grandi <fgrandi30@gmail.com>
2019-02-07 21:43:44 +0100

    Create setup.js

---
+++ js/setup.js
@@ -0,0 +1,9 @@
+function loadFile(...paths) {
+  for (let path of paths) {
+    let e = document.createElement('script');
+    e.src = path;
+    document.getElementsByTagName('head')[0].appendChild(e);
+  }
+}
+
+loadFile('http://code.jquery.com/jquery-3.3.1.min.js');
