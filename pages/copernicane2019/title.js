e44e9674f7eed9aa12f8f6d8367db3a68e0cc901
Federico Grandi <fgrandi30@gmail.com>
2019-02-07 21:46:35 +0100

    Update title.js
    Removed JQuery declaration

--- pages/copernicane2019/title.js
+++ pages/copernicane2019/title.js
@@ -1,7 +1,3 @@
-var $ = document.createElement('script');
-$.src = '//code.jquery.com/jquery-1.11.0.min.js';
-document.getElementsByTagName('head')[0].appendChild($);
-
 $().ready(() => {
   $('.project-name').first().text("Copernicane 2019");
 });
